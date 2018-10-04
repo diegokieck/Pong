9e09630d10496b55b89469f453c6b70d651ea4d3
Diego Kieckbusch <diegokieck@gmail.com>
2018-10-04 11:38:36 -0300

    added collision with paddles

--- pong.py
+++ pong.py
@@ -39,7 +39,47 @@ def drawPaddle(paddle):
 def drawBall(ball):
     pygame.draw.rect(DISPLAYSURF, WHITE, ball)
 
+def moveBall(ball, ballDirX, ballDirY):
+	ball.x+=ballDirX
+	ball.y+=ballDirY
+	return ball
 
+def ballEdgeDetector(ball, ballDirX,ballDirY):
+	if ball.top == LINETHICKNESS or ball.bottom == WINDOWHEIGHT - LINETHICKNESS:
+		ballDirY = ballDirY*-1
+	if ball.left ==LINETHICKNESS or ball.right ==WINDOWWIDTH -LINETHICKNESS:
+		ballDirX = ballDirX*-1
+	return ballDirX, ballDirY
+
+def movePaddle(paddle, event):
+	if(event>0):
+		paddle.y+=1
+	if(event<0):
+		paddle.y-=1
+	return paddle
+def computerPaddle(ball, ballDirX, paddle):
+	#after hiting the ball the paddle will move to the center of the screen
+	if(ballDirX==-1):
+		if(paddle.centery>WINDOWHEIGHT/2):
+			paddle.y-=1
+		if(paddle.centery<WINDOWHEIGHT/2):	
+			paddle.y+=1
+	if(ballDirX==1):
+		if(paddle.centery>ball.centery):
+			paddle.y-=1
+		if(paddle.centery<ball.centery):	
+			paddle.y+=1
+
+	return paddle
+def paddleDetection(ball, paddle1, paddle2):
+	if(ball.x==LINETHICKNESS/2+PADDLEOFFSET and paddle1.top<ball.y<paddle1.bottom):
+		return int(-1)
+	elif(ball.x==WINDOWWIDTH- (LINETHICKNESS*1.5 +PADDLEOFFSET) and paddle2.top<ball.y<paddle2.bottom):
+		return int(-1)
+	else:
+		return 1 
+def scoring(ball):
+	if
 def main():
 	pygame.init()
 	global DISPLAYSURF
@@ -66,18 +106,28 @@ def main():
 	drawPaddle(paddle2)
 	drawBall(ball)
 
+	#initial ballDir
+	ballDirX=1
+	ballDirY=1
 
+	pygame.mouse.set_visible(0)
 	while(True): #main game loop
 		for event in pygame.event.get():
 			if event.type ==QUIT:
 				pygame.quit()
 				sys.exit()
-			print(event)
+			
+			if event.type ==MOUSEMOTION:
+				paddle1.y = event.pos[1]
 
-		#drawArena()
-		#drawPaddle(paddle1)
-		#drawPaddle(paddle2)
-		#drawBall(ball)
+		drawArena()
+		drawPaddle(paddle1)
+		drawPaddle(paddle2)
+		ballDirX = ballDirX*paddleDetection(ball, paddle1, paddle2)
+		ballDirX, ballDirY = ballEdgeDetector(ball, ballDirX, ballDirY)
+		ball = moveBall(ball, ballDirX, ballDirY)
+		paddle2 = computerPaddle(ball, ballDirX, paddle2)
+		drawBall(ball)
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
 
